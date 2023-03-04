b3652fcad767fca7f109623960855fefa4a25e4d
Artur Carvalho <111922579+ArturCarvalho0@users.noreply.github.com>
2023-03-04 18:03:21 -0300

    Atualização

---
+++ Funções/Funções fábrica(Factory Functions)/main.js
@@ -0,0 +1,42 @@
+// Factory function (Função fábrica)
+// Constructor function (Função construtora)
+function criaPessoa(nome, sobrenome, a, p) {
+  return {
+    nome,
+    sobrenome,
+
+    // Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+
+    // Setter
+    set nomeCompleto(valor) {
+      valor = valor.split(' ');
+      this.nome = valor.shift();
+      this.sobrenome = valor.join(' ');
+    },
+
+    fala(assunto = 'falando sobre NADA') {
+      return `${this.nome} está ${assunto}.`;
+    },
+
+    altura: a,
+    peso: p,
+
+    // Getter
+    get imc() {
+      const indice = this.peso / (this.altura ** 2);
+      return indice.toFixed(2);
+    }
+  };
+}
+
+const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80);
+const p2 = criaPessoa('João', 'Otávio', 1.90, 57);
+const p3 = criaPessoa('Junior', 'Otávio', 1.5, 110);
+
+console.log(p1.imc);
+console.log(p2.imc);
+console.log(p3.imc);
+
