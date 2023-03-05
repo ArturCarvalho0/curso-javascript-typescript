0c1b04a436c237dc7409d954335586a4022a91ed
Artur Carvalho <111922579+ArturCarvalho0@users.noreply.github.com>
2023-03-05 11:43:28 -0300

    Exercicio factory function

---
+++ Funções/Praticando com factory functions/assets/js/main.js
@@ -0,0 +1,73 @@
+//eval entende uma string como código javascript
+
+function criaCalculadora() {
+  return {
+    display: document.querySelector(".display"),
+
+    inicia() {
+      this.cliqueBotoes();
+      this.precionaEnter();
+    },
+
+    precionaEnter() {
+      this.display.addEventListener("keyup", (e) => {
+        if (e.keyCode === 13) {
+          this.realizaConta();
+        }
+      });
+    },
+
+    clearDisplay() {
+      this.display.value = "";
+    },
+
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
+    realizaConta() {
+      let conta = this.display.value;
+
+      try {
+        conta = eval(conta);
+        if (!conta) {
+          alert("Conta inválida");
+          return;
+        }
+        this.display.value = conta;
+      } catch (e) {
+        alert("Conta invalida");
+        return;
+      }
+    },
+
+    cliqueBotoes() {
+      document.addEventListener("click", (e) => {
+        const el = e.target;
+
+        if (el.classList.contains("btn-num")) {
+          this.btnParaDisplay(el.innerText);
+        }
+
+        if (el.classList.contains("btn-clear")) {
+          this.clearDisplay();
+        }
+
+        if (el.classList.contains("btn-del")) {
+          this.apagaUm();
+        }
+
+        if (el.classList.contains("btn-eq")) {
+          this.realizaConta();
+        }
+      });
+    },
+
+    btnParaDisplay(valor) {
+      this.display.value += valor;
+    },
+  };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
