4d70bce264df12ad6d151f58fedc2347fb1f95e4
ArturCarvalho0 <artur.carvalho0@outlook.com>
2023-03-24 00:01:49 -0300

    modulos node

---
+++ Node/modulos2/app.js
@@ -0,0 +1,4 @@
+const Cachorro = require("./mod");
+
+const dog = new Cachorro("Bill")
+dog.latir()
---
+++ Node/modulos2/mod.js
@@ -0,0 +1,8 @@
+module.exports = class Cachorro {
+  constructor(nome) {
+    this.nome = nome;
+  }
+  latir() {
+    console.log(`${this.nome} está latindo`);
+  }
+};
