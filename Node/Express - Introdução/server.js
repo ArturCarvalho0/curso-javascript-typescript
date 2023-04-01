ffbdd4b14cc3c4826cafe2a260b92bd3d259a918
ArturCarvalho0 <artur.carvalho0@outlook.com>
2023-03-31 22:43:49 -0300

    express introdução

---
+++ Node/Express - Introdução/server.js
@@ -0,0 +1,26 @@
+// CRUD -> Create,    Read,    Update,    Delete
+//         Criar      Ler      Atualizar  Apagar
+//         Post       Get      Put        Delete
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+  <form method="post" action="/">
+    Nome: <input type="text" name="nome">
+    <button>Enviar</button>
+  </form>
+  `);
+});
+
+app.post("/", (req, res) => {
+  res.send("recebi os dados");
+});
+
+app.get("/contato", (req, res) => {
+  res.send("Obrigado por entrar em Contato");
+});
+
+app.listen(3000, () => {
+  console.log("Acessar http://localhost:3000");
+});
