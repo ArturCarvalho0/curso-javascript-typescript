88fb0978cc41c54e5f67d528ff7b9c9623e76cab
ArturCarvalho0 <artur.carvalho0@outlook.com>
2023-04-06 22:26:37 -0300

    modelo completo

---
+++ Node/Modelo Completo/src/middlewares/middleware.js
@@ -0,0 +1,15 @@
+exports.middlewareGlobal = (req, res, next) => {
+  res.locals.umaVariavelLocal = "Este é o valor da variável local";
+  next();
+};
+
+exports.checkCsrfError = (err, req, res, next) => {
+  if (err && err.code === "EBADCSRFTOKEN") {
+    return res.render("404");
+  }
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+};
